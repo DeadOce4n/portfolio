c936db71f7eeb4ff2a96826d7c4408905a93cb85
ThyDevourer <admin@suprachat.net>
2022-04-02 20:05:51 -0700

    Navbar overhaul

--- js/index.js
+++ js/index.js
@@ -18,12 +18,22 @@ const setupTextInterval = () => {
 
 const setupNavObserver = () => {
   const navbar = document.querySelector('.navbar')
+  const burger = document.querySelector('.burger')
+  const navLinks = document.querySelectorAll('.nav-link')
   const callback = (entries, observer) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         navbar.classList.add('even')
+        burger.classList.add('even')
+        for (const navLink of navLinks) {
+          navLink.classList.add('even')
+        }
       } else {
         navbar.classList.remove('even')
+        burger.classList.remove('even')
+        for (const navLink of navLinks) {
+          navLink.classList.remove('even')
+        }
       }
     })
   }
@@ -41,7 +51,37 @@ const setupNavObserver = () => {
   }
 }
 
+const setupBurger = () => {
+  const burger = document.querySelector('.burger')
+  const menu = document.querySelector('.nav-menu')
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('visible')
+    menu.classList.toggle('visible')
+  })
+}
+
+const setupNavEvents = () => {
+  const navLinks = document.querySelectorAll('.nav-link')
+  const navMenu = document.querySelector('.nav-menu')
+  const scrollContainer = document.querySelector('.scroll-container')
+
+  for (const navLink of navLinks) {
+    navLink.addEventListener('click', () => {
+      navMenu.classList.toggle('visible')
+    })
+  }
+
+  scrollContainer.addEventListener('scroll', () => {
+    if (navMenu.classList.contains('visible')) {
+      navMenu.classList.remove('visible')
+    }
+  })
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   setupTextInterval()
   setupNavObserver()
+  setupBurger()
+  setupNavEvents()
 })
